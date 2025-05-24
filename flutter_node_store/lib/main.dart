0f4e0ed10cc994844c146bdef872df7caa81c8a7
chaiyaphat supharak <chaiyaphat2024@gmail.com>
2025-05-24 14:28:40 +0700

    feat: check initial route

--- flutter_node_store/lib/main.dart
+++ flutter_node_store/lib/main.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_node_store/app_router.dart';
-import 'package:flutter_node_store/screen/welcome/welcome_screen.dart';
 import 'package:flutter_node_store/themes/styles.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+var initialRoute;
+
+void main() async {
+  // ต้องเรียกใช้ WidgetsFlutterBinding.ensureInitialized()
+  // เพื่อให้สามารถเรียกใช้ SharedPreferences ได้
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // สร้างตัวแปร pref เพื่อเรียกใช้ SharedPreferences
+  SharedPreferences pref =
+      await SharedPreferences.getInstance();
+
+  // ตรวจสอบว่าเคยแสดง Intro แล้วหรือยัง
+  if (pref.getBool('welcomeStatus') == true) {
+    // ถ้าเคยแสดง Intro แล้ว ให้ไปยังหน้า Login
+    initialRoute = AppRouter.login;
+  } else {
+    // ถ้ายังไม่เคยแสดง Intro ให้ไปยังหน้า Welcome
+    initialRoute = AppRouter.welcome;
+  }
   runApp(const MyApp());
 }
 
@@ -15,7 +33,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      initialRoute: AppRouter.welcome,
+      initialRoute: initialRoute,
       routes: AppRouter.routes,
     );
   }
--- flutter_node_store/lib/screen/welcome/welcome_screen.dart
+++ flutter_node_store/lib/screen/welcome/welcome_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_node_store/app_router.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -17,12 +18,16 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
 
   // ฟังก์ชันเมื่อจบการแสดง Intro
   void _onIntroEnd(context) async {
-    // ไปยังหน้า Login แบบเปิดซ้อนทับหน้าเดิม
-    // Navigator.pushNamed(
-    //   context,
-    //   AppRouter.login,
-    // ); // เปิดแบบปกติมีปุ่มย้อนกลับ
+    //set ค่า เพื่อ ไม่ให้เข้าหน้านี้อีกรอบ
+    SharedPreferences pref =
+        await SharedPreferences.getInstance();
+    pref.setBool('welcomeStatus', true);
 
+    // ไปยังหน้า Login
+    Navigator.pushReplacementNamed(
+      context,
+      AppRouter.login,
+    );
     // ไปยังหน้า Login แบบเปิดแบบไม่มีปุ่มย้อนกลับ แทนที่หน้าเดิม
     Navigator.pushReplacementNamed(
       context,
